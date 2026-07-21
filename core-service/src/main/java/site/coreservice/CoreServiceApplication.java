a130c72826032300ad68fd0b0566d26dfa7d9765
efdao <efdao@naver.com>
2026-07-21 12:19:08 +0900

    #36 (all) :bug: 패키지 스캔 문제 해결

--- core-service/src/main/java/site/coreservice/CoreServiceApplication.java
+++ core-service/src/main/java/site/coreservice/CoreServiceApplication.java
@@ -3,7 +3,10 @@ package site.coreservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "site.coreservice",
+    "site.common"
+})
 public class CoreServiceApplication {
 
     public static void main(String[] args) {
