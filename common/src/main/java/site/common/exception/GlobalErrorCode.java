e36ee61378cd4f06b40336ba47c17ab168ca5a18
Sunguk Yang (Kelly) <kelly6bf@gmail.com>
2026-07-22 16:21:58 +0900

    (common) 비즈니스 예외 추상화 객체 생성 및 ErrorCode에 HttpStatus 값 추가 (#50)
    * #48 (common) :sparkles: ErrorCode에 Http Status 필드 추가
    
    * #48 (common) :sparkles: BusinessException 클래스 추가 및 관련 객체에 적용
    
    * #48 (common) :sparkles: ApiResponse 실패 정적 메서드 수정 및 GlobalExceptionHandler 내부에 로깅 로직 추가
    
    * #48 (common) :sparkles: 리소스 없음 에러 코드 정의 및 적용
    
    * #48 (common) :wrench: JSON 역질럴화시 필드 순서를 알파벳으로 정렬하는 옵션 비활성화

--- common/src/main/java/site/common/exception/GlobalErrorCode.java
+++ common/src/main/java/site/common/exception/GlobalErrorCode.java
@@ -3,14 +3,17 @@ package site.common.exception;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public enum GlobalErrorCode implements ErrorCode {
 
-    INTERNAL_SERVER_APPLICATION_ERROR("GERR-0001", "서버 애플리케이션에 예기치 못한 문제가 발생했습니다."),
+    INTERNAL_SERVER_APPLICATION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GERR-0001", "서버 애플리케이션에 예기치 못한 문제가 발생했습니다."),
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "GERR-0002", "존재하지 않는 리소스입니다.")
     ;
 
+    private final HttpStatus status;
     private final String value;
     private final String message;
 }
