a7950bf9cd10331b4e7eec5d5fb8f17b2d7d0add
kelly6bf <kelly6bf@gmail.com>
2026-07-22 14:15:22 +0900

    #48 (common) :sparkles: ErrorCode에 Http Status 필드 추가

--- common/src/main/java/site/common/exception/GlobalErrorCode.java
+++ common/src/main/java/site/common/exception/GlobalErrorCode.java
@@ -3,14 +3,16 @@ package site.common.exception;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public enum GlobalErrorCode implements ErrorCode {
 
-    INTERNAL_SERVER_APPLICATION_ERROR("GERR-0001", "서버 애플리케이션에 예기치 못한 문제가 발생했습니다."),
+    INTERNAL_SERVER_APPLICATION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GERR-0001", "서버 애플리케이션에 예기치 못한 문제가 발생했습니다."),
     ;
 
+    private final HttpStatus status;
     private final String value;
     private final String message;
 }
