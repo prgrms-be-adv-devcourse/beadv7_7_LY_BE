9c97975c863c29775175769b482d627b96cf1ef3
kelly6bf <kelly6bf@gmail.com>
2026-07-23 21:28:46 +0900

    #60 (member) :sparkles: MemberErrorCode & MemberException 생성

---
+++ member-service/src/main/java/site/memberservice/exception/MemberErrorCode.java
@@ -0,0 +1,21 @@
+package site.memberservice.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import site.common.exception.ErrorCode;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+public enum MemberErrorCode implements ErrorCode {
+
+    INVALID_MEMBER_INFO(BAD_REQUEST, "MERR-0001", "유효하지 않은 회원 정보입니다.")
+    ;
+
+    private final HttpStatus status;
+    private final String value;
+    private final String message;
+}
