f4fcf04c102cd7c9b8805fa370565a3c178d687a
efdao <efdao@naver.com>
2026-07-23 17:10:28 +0900

    #13 (auction) :sparkles: 경매 시작 처리용 스케줄러 구현

--- core-service/src/main/java/site/coreservice/auction/domain/AuctionRepository.java
+++ core-service/src/main/java/site/coreservice/auction/domain/AuctionRepository.java
@@ -1,9 +1,14 @@
 package site.coreservice.auction.domain;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface AuctionRepository {
+
     Auction save(Auction auction);
 
     Optional<Auction> findById(Long id);
+
+    List<Auction> findAllScheduledToStart(LocalDateTime threshold);
 }
