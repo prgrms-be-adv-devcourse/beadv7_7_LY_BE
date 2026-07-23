437e20f80b3bcd6bffd9dd9eea83e0406b05a24d
OHJOUYEON <hyperclutter@gmail.com>
2026-07-23 12:18:52 +0900

    32 (pointwallet) hold 관련 테스트 추가

---
+++ docker/local/init-sql/02_hold_schema.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE hold (
+    id              BIGINT          NOT NULL AUTO_INCREMENT,
+    auction_id      BIGINT          NOT NULL,
+    user_id         BIGINT          NOT NULL,
+    amount          DECIMAL(19, 0)  NOT NULL,
+    held_at         DATETIME(6)     NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT ukHoldAuctionId UNIQUE (auction_id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
