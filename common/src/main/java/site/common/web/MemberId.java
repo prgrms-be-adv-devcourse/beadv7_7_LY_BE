0f58177477dab6c4af49ac6241e1a09890e9437c
Kim DongHyun <150654090+efdao@users.noreply.github.com>
2026-07-24 10:38:45 +0900

    #57 (core) :sparkles: 헤더에서 member id 추출하는 MemberId, OptionalMemberId 어노테이션 추가 (#59)
    * #21 (core) :sparkles: 헤더에서 member id 추출하는 MemberId, OptionalMemberId 어노테이션 생성
    
    * #57 (core) :sparkles: X-Member-Id 헤더명 반영, OptionalMemberId 제거
    
    * #57 (auction) :sparkles: MemberId 디렉토리 이동 core.global.web -> common.web

---
+++ common/src/main/java/site/common/web/MemberId.java
@@ -0,0 +1,14 @@
+package site.common.web;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// 헤더 없으면 BusinessException(GlobalErrorCode.MEMBER_ID_HEADER_REQUIRED)
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MemberId {
+
+    String HEADER_NAME = "X-Member-Id";
+}
---
+++ common/src/main/java/site/common/web/MemberIdWebMvcConfig.java
@@ -0,0 +1,16 @@
+package site.common.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class MemberIdWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new MemberIdArgumentResolver());
+    }
+}
