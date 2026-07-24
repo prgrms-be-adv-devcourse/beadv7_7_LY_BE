6750457fd907a206f58b0f700d3e19ac0d387cba
kelly6bf <kelly6bf@gmail.com>
2026-07-24 16:24:04 +0900

    #60 (member) :sparkles: 회원 가입 컨트롤러 메서드 구현

--- member-service/src/main/java/site/memberservice/member/presentation/MemberController.java
+++ member-service/src/main/java/site/memberservice/member/presentation/MemberController.java
@@ -1,7 +1,26 @@
 package site.memberservice.member.presentation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.common.response.ApiResponse;
+import site.memberservice.member.application.MemberService;
+import site.memberservice.member.presentation.request.RegisterRequest;
 
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 @RestController
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/members")
+    public ResponseEntity<ApiResponse<Void>> register(@RequestBody RegisterRequest request) {
+        memberService.register(request.toCommand());
+
+        return ResponseEntity.ok(ApiResponse.success());
+    }
 }
