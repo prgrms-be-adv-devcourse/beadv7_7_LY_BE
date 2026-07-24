3a33463db44ef4cb94024dfb721aacc9ac3af036
kelly6bf <kelly6bf@gmail.com>
2026-07-24 17:46:35 +0900

    #60 (member) :truck: 기존 MemberApiController 클래스명에 Api 추가 웹 & 앱 제공용 API는 API, 내부 시스템 제공용 API는 Internal을 사용해 둘을 구분함

--- member-service/src/main/java/site/memberservice/member/presentation/api/MemberApiController.java
+++ member-service/src/main/java/site/memberservice/member/presentation/api/MemberApiController.java
@@ -1,19 +1,22 @@
-package site.memberservice.member.presentation;
+package site.memberservice.member.presentation.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.common.response.ApiResponse;
+import site.common.web.MemberId;
 import site.memberservice.member.application.MemberService;
+import site.memberservice.member.application.dto.AddressDto;
 import site.memberservice.member.presentation.request.RegisterRequest;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 @RestController
-public class MemberController {
+public class MemberApiController {
 
     private final MemberService memberService;
 
@@ -22,5 +25,12 @@ public class MemberController {
         memberService.register(request.toCommand());
 
         return ResponseEntity.ok(ApiResponse.success());
+    }
+
+    @GetMapping("/members/address")
+    public ResponseEntity<ApiResponse<AddressDto>> getMemberAddress(@MemberId final Long memberId) {
+        final AddressDto memberAddress = memberService.getMemberAddress(memberId);
+
+        return ResponseEntity.ok(ApiResponse.success(memberAddress));
     }
 }
