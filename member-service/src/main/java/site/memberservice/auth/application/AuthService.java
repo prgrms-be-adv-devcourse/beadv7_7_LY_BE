412b84ba0c2b727962b5f1a13af4d2008ae31634
kelly6bf <kelly6bf@gmail.com>
2026-07-27 11:30:21 +0900

    #80 (auth) :sparkles: 로그인 기능 구현

---
+++ member-service/src/main/java/site/memberservice/auth/application/AuthService.java
@@ -0,0 +1,35 @@
+package site.memberservice.auth.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import site.memberservice.auth.application.dto.LoginCommand;
+import site.memberservice.auth.application.dto.LoginResult;
+import site.memberservice.auth.domain.AuthTokenProvider;
+import site.memberservice.auth.exception.AuthException;
+import site.memberservice.member.application.MemberService;
+import site.memberservice.member.domain.Member;
+
+import static java.lang.String.*;
+import static site.memberservice.auth.exception.AuthErrorCode.INVALID_CREDENTIALS;
+
+@RequiredArgsConstructor
+@Service
+public class AuthService {
+
+    // TODO : #80 회원 모듈과 인중 모듈 분리를 고려해 파이널에서 회원 API를 호출하는 방식으로 변경
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthTokenProvider authTokenProvider;
+
+    public LoginResult login(final LoginCommand command) {
+        final Member member = memberService.findMember(command.email())
+            .orElseThrow(() -> new AuthException(INVALID_CREDENTIALS, format("존재하지 않는 회원의 이메일입니다. input: %s", command.email())));
+
+        if (!passwordEncoder.matches(command.password(), member.getPassword())) {
+            throw new AuthException(INVALID_CREDENTIALS, "유효하지 않는 회원 비밀번호입니다.");
+        }
+
+        return new LoginResult(authTokenProvider.createToken(member.getId()).getValue());
+    }
+}
--- member-service/src/main/java/site/memberservice/auth/exception/AuthErrorCode.java
+++ member-service/src/main/java/site/memberservice/auth/exception/AuthErrorCode.java
@@ -14,6 +14,7 @@ public enum AuthErrorCode implements ErrorCode {
 
     INVALID_AUTH_TOKEN(UNAUTHORIZED, "AERR-0001", "유효하지 않은 인증 토큰입니다."),
     EXPIRED_AUTH_TOKEN(UNAUTHORIZED, "AERR-0002", "이미 만료된 인증 토큰입니다."),
+    INVALID_CREDENTIALS(UNAUTHORIZED, "AERR-0003", "이메일 또는 비밀번호가 올바르지 않습니다."),
     ;
 
     private final HttpStatus status;
