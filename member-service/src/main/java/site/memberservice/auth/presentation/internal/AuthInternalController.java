a5d8298926970cd1bbc0be14a831473062dc0a27
kelly6bf <kelly6bf@gmail.com>
2026-07-27 12:48:52 +0900

    #80 (auth) :sparkles: 토큰 검증 컨트롤러 로직 구현

---
+++ member-service/src/main/java/site/memberservice/auth/presentation/internal/AuthInternalController.java
@@ -0,0 +1,28 @@
+package site.memberservice.auth.presentation.internal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.common.response.ApiResponse;
+import site.memberservice.auth.application.AuthService;
+
+@RequiredArgsConstructor
+@RequestMapping("/internal/v1/auth")
+@RestController
+public class AuthInternalController {
+
+    private final AuthService authService;
+
+    @GetMapping("/validate-token")
+    public ResponseEntity<ApiResponse<Void>> validateAuthToken(@RequestHeader(HttpHeaders.AUTHORIZATION) final String authToken) {
+        final Long authenticatedMemberId = authService.validateAuthToken(authToken);
+
+        return ResponseEntity.ok()
+            .header("X-Member-Id", String.valueOf(authenticatedMemberId))
+            .body(ApiResponse.success());
+    }
+}
