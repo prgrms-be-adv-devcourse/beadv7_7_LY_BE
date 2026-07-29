f9dd608cd970ed4e409d1ab27892d8c2e5191062
OHJOUYEON <hyperclutter@gmail.com>
2026-07-29 15:46:12 +0900

    #135 (pointwallet) :sparkles: 재커밋

--- core-service/src/main/java/site/coreservice/pointwallet/wallet/presentation/dto/WalletBalanceResponse.java
+++ core-service/src/main/java/site/coreservice/pointwallet/wallet/presentation/dto/WalletBalanceResponse.java
@@ -1,4 +1,9 @@
 package site.coreservice.pointwallet.wallet.presentation.dto;
+import java.math.BigDecimal;
 
-public class WalletBalanceResponse {
+public record WalletBalanceResponse(BigDecimal availableBalance) {
+
+    public static WalletBalanceResponse from(BigDecimal availableBalance) {
+        return new WalletBalanceResponse(availableBalance);
+    }
 }
