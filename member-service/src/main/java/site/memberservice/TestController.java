00775f825880d7ccf57123201a07220523ebd75c
kelly6bf <kelly6bf@gmail.com>
2026-07-30 09:33:05 +0900

    cd test 02

---
+++ member-service/src/main/java/site/memberservice/TestController.java
@@ -0,0 +1,19 @@
+package site.memberservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.common.web.MemberId;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/v1/members/white")
+    public String white() {
+        return "member-service 인증 필요없는 white 호출 성공";
+    }
+
+    @GetMapping("/api/v1/members/black")
+    public String black(@MemberId Long memberId) {
+        return "member-service 인증 필요한 black 호출 성공 - " + memberId;
+    }
+}
