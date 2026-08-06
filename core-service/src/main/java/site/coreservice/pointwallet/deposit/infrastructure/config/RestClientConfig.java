894eafac9779ae5aed92dfa45c4ac4c9c84e63b9
OHJOUYEON <hyperclutter@gmail.com>
2026-08-06 13:44:41 +0900

    #165 :recycle: pg연동 관련 멱등성 키, 타임아웃 로직 추가

--- core-service/src/main/java/site/coreservice/pointwallet/deposit/infrastructure/config/RestClientConfig.java
+++ core-service/src/main/java/site/coreservice/pointwallet/deposit/infrastructure/config/RestClientConfig.java
@@ -1,7 +1,9 @@
 package site.coreservice.pointwallet.deposit.infrastructure.config;
-
+import java.net.http.HttpClient;
+import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -9,6 +11,15 @@ public class RestClientConfig {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.create();
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(3))
+                .build();
+
+        JdkClientHttpRequestFactory requestFactory = new JdkClientHttpRequestFactory(httpClient);
+        requestFactory.setReadTimeout(Duration.ofSeconds(10));
+
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .build();
     }
 }
