6b008bad2a288ba327850a7c60b4b6a1df53af65
OHJOUYEON <hyperclutter@gmail.com>
2026-08-07 16:10:59 +0900

    #185 (pointwallet) :bug: 토스관련 에너테이션 추가

--- pointwallet-service/src/main/java/site/pointwalletservice/PointwalletServiceApplication.java
+++ pointwallet-service/src/main/java/site/pointwalletservice/PointwalletServiceApplication.java
@@ -1,11 +1,14 @@
 package site.pointwalletservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import site.pointwalletservice.deposit.infrastructure.toss.TossPaymentsProperties;
 
 @SpringBootApplication(scanBasePackages = {
         "site.common",
         "site.pointwalletservice"
 })
+@EnableConfigurationProperties(TossPaymentsProperties.class)
 public class PointwalletServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PointwalletServiceApplication.class, args);
