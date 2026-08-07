55225efa8a82756fb0ec88baf39ab7af5f52b2d5
chanyong <137212216+chanyong1027@users.noreply.github.com>
2026-08-07 09:44:01 +0900

    #172 (product) 🚚 global 패키지 없애고 config 패키지로 정리
    - global 하위에 config 외 다른 요소가 없어 계층을 한 단계 줄임
    - 추후 global 성격 요소가 생기면 그때 다시 도입

--- product-service/src/main/java/site/productservice/config/KafkaErrorHandlerConfig.java
+++ product-service/src/main/java/site/productservice/config/KafkaErrorHandlerConfig.java
@@ -1,4 +1,4 @@
-package site.productservice.global.config;
+package site.productservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
