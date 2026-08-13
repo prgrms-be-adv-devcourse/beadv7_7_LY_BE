7c19f533b2f05e48ed1746a5c93e1631a31efe99
kelly6bf <kelly6bf@gmail.com>
2026-08-13 12:15:44 +0900

    #229 (member) : :sparkles: 회원 제재 내역 조회 Repository 로직 구현

--- member-service/src/main/java/site/memberservice/member/infrastructure/repository/MemberRestrictionRepositoryImpl.java
+++ member-service/src/main/java/site/memberservice/member/infrastructure/repository/MemberRestrictionRepositoryImpl.java
@@ -2,8 +2,12 @@ package site.memberservice.member.infrastructure.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import site.memberservice.member.domain.Member;
 import site.memberservice.member.domain.MemberRestriction;
 import site.memberservice.member.domain.repository.MemberRestrictionRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
@@ -14,5 +18,10 @@ public class MemberRestrictionRepositoryImpl implements MemberRestrictionReposit
     @Override
     public MemberRestriction save(final MemberRestriction memberRestriction) {
         return memberRestrictionJpaRepository.save(memberRestriction);
+    }
+
+    @Override
+    public List<MemberRestriction> findActiveByMember(final Member member, final LocalDateTime now) {
+        return memberRestrictionJpaRepository.findByMemberAndRestrictedAtLessThanEqualAndRestrictedUntilAfter(member, now, now);
     }
 }
