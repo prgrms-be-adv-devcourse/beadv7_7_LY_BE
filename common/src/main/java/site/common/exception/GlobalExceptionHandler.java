7b636bc84a9c21cb4bc4c1b59dfec9afa52f6553
Kim DongHyun <150654090+efdao@users.noreply.github.com>
2026-08-24 15:51:02 +0900

    (common) :sparkles: 4xx-warn 그외-error로 로그레벨 수정 (#275)

--- common/src/main/java/site/common/exception/GlobalExceptionHandler.java
+++ common/src/main/java/site/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,5 @@
 package site.common.exception;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.core.Ordered;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 import site.common.response.ApiResponse;
+
 import static site.common.exception.GlobalErrorCode.INTERNAL_SERVER_APPLICATION_ERROR;
 import static site.common.exception.GlobalErrorCode.RESOURCE_NOT_FOUND;
 import static site.common.response.ApiResponse.fail;
@@ -20,21 +22,47 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ApiResponse<Void>> handleBusinessException(final BusinessException e) {
-        log.error("{} 발생!", e.getClass().getSimpleName(), e);
-
         final ErrorCode errorCode = e.getErrorCode();
+        // 4xx의 경우 클라이언트 요청 문제로 보고 Warn
+        final LogLevel logLevel =
+            errorCode.getStatus().is4xxClientError() ? LogLevel.WARN : LogLevel.ERROR;
+
+        logException(logLevel, errorCode, e);
+
         return ResponseEntity.status(errorCode.getStatus()).body(fail(errorCode));
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ApiResponse<Void>> handleNoResourceFoundException(final NoResourceFoundException e) {
-        log.warn("{} 발생!", e.getClass().getSimpleName(), e);
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleNoResourceFoundException(
+        final NoResourceFoundException e) {
+        logException(LogLevel.WARN, RESOURCE_NOT_FOUND, e);
+
         return ResponseEntity.status(RESOURCE_NOT_FOUND.getStatus()).body(fail(RESOURCE_NOT_FOUND));
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(final Exception e) {
-        log.error("{} 발생!", e.getClass().getSimpleName(), e);
-        return ResponseEntity.status(INTERNAL_SERVER_APPLICATION_ERROR.getStatus()).body(fail(INTERNAL_SERVER_APPLICATION_ERROR));
+        logException(LogLevel.ERROR, INTERNAL_SERVER_APPLICATION_ERROR, e);
+
+        return ResponseEntity.status(INTERNAL_SERVER_APPLICATION_ERROR.getStatus())
+            .body(fail(INTERNAL_SERVER_APPLICATION_ERROR));
+    }
+
+    private void logException(final LogLevel logLevel, final ErrorCode errorCode,
+        final Throwable e) {
+        final String message = String.format("[type=%s] [code=%s] [status=%d] message=\"%s\"",
+            e.getClass().getSimpleName(),
+            errorCode.getValue(),
+            errorCode.getStatus().value(),
+            e.getMessage());
+
+        switch (logLevel) {
+            case WARN -> log.warn(message, e);
+            case ERROR -> log.error(message, e);
+        }
+    }
+
+    private enum LogLevel {
+        WARN, ERROR
     }
 }
