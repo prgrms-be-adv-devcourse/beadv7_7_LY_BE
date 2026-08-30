517e5774be39bae6f69070ad7bbcb3e1cfe6b781
kelly6bf <kelly6bf@gmail.com>
2026-08-30 15:18:55 +0900

    #340 (auth) : :config: Argon2id의 암호화 설정 파라미터를 OWASP 최소 권장치 이상으로 수정

--- member-service/src/main/java/site/memberservice/global/security/SecurityConfig.java
+++ member-service/src/main/java/site/memberservice/global/security/SecurityConfig.java
@@ -8,9 +8,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
+    private static final int MEMORY_KIB = 9216; // 9 MIB
+    private static final int ITERATIONS = 4;
+    private static final int PARALLELISM = 1;
+    private static final int SALT_LENGTH = 16;
+    private static final int HASH_LENGTH = 32;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // 기본 권장 설정 (Salt 16bytes, Hash 32bytes, Parallelism 1, Memory 16MB, Iterations 2)
-        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+        return new Argon2PasswordEncoder(SALT_LENGTH, HASH_LENGTH, PARALLELISM, MEMORY_KIB, ITERATIONS);
     }
 }
