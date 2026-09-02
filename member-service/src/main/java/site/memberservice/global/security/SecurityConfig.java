5e2f28514fca8bb4520889805181a3bde2cfa399
kelly6bf <kelly6bf@gmail.com>
2026-09-02 14:37:30 +0900

    #359 (common) : :sparkles: Argon2id 세마포어 빈 등록 로직 구현

--- member-service/src/main/java/site/memberservice/global/security/SecurityConfig.java
+++ member-service/src/main/java/site/memberservice/global/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.concurrent.Semaphore;
+
 @Configuration
 public class SecurityConfig {
 
@@ -14,8 +16,15 @@ public class SecurityConfig {
     private static final int SALT_LENGTH = 16;
     private static final int HASH_LENGTH = 32;
 
+    private static final int ARGON2_MAX_CONCURRENCY = Runtime.getRuntime().availableProcessors();
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new Argon2PasswordEncoder(SALT_LENGTH, HASH_LENGTH, PARALLELISM, MEMORY_KIB, ITERATIONS);
+    }
+
+    @Bean
+    public Semaphore argon2ConcurrencyLimiter() {
+        return new Semaphore(ARGON2_MAX_CONCURRENCY);
     }
 }
